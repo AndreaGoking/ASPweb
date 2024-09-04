caa949fbaa4c8a1a130a4b2f99db149108c87ffc
AndreaGoking <AndreaGoking@gmail.com>
2024-09-04 15:01:06 +0700

    Hoan thanh bai tap 05

---
+++ Baitap05/Baitap05/Models/TheLoaiViewModelcs.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap05.Models
+{
+    public class TheLoaiViewModal
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
