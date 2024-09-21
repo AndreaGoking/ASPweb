d4c2ac43c509bfb34fea2d76b6205c751fb93097
AndreaGoking <AndreaGoking@gmail.com>
2024-09-21 12:43:18 +0700

    Hoan thanh bai tap 07

---
+++ BaiTap07/Controllers/TheLoaiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TheLoai()
+        {
+            return View();
+        }
+    }
+}
