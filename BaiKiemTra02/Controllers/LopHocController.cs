1da870dfe701d93b42398da9b64bf0e6b64ad5d9
AndreaGoking <AndreaGoking@gmail.com>
2024-09-28 15:03:45 +0700

    Hoan thanh bai kiem tra 02

---
+++ BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,121 @@
+﻿using BaiKiemTra02.Data;
+using BaiKiemTra02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public LopHocController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // Hiển thị danh sách lớp học
+        public IActionResult Index()
+        {
+            var lophoc = _db.LopHoc.ToList();
+            ViewBag.LopHoc = lophoc;
+
+            return View(lophoc);
+        }
+        // GET: Tạo mới lớp học
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tạo mới lớp học
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.LopHoc.Add(lophoc);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lophoc);
+        }
+
+        // GET: Chỉnh sửa lớp học
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.LopHoc.Find(id);
+            if (lophoc == null)
+            {
+                return NotFound();
+            }
+            return View(lophoc);
+        }
+
+        // POST: Chỉnh sửa lớp học
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.LopHoc.Update(lophoc);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lophoc);
+        }
+
+        // GET: Xóa lớp học
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.LopHoc.Find(id);
+            if (lophoc == null)
+            {
+                return NotFound();
+            }
+            return View(lophoc);
+        }
+
+        // POST: Xác nhận xóa lớp học
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var lophoc = _db.LopHoc.Find(id);
+            if (lophoc == null)
+            {
+                return NotFound();
+            }
+            _db.LopHoc.Remove(lophoc);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Xem chi tiết lớp học
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.LopHoc.Find(id);
+            if (lophoc == null)
+            {
+                return NotFound();
+            }
+            return View(lophoc);
+        }
+
+    }
+}
---
+++ BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên lớp học là bắt buộc")]
+        [Display(Name = "Tên Lớp Học")]
+        [StringLength(100, ErrorMessage = "Tên lớp học không được vượt quá 100 ký tự")]
+        public string TenLopHoc { get; set; }
+
+        [Required(ErrorMessage = "Năm nhập học là bắt buộc")]
+        [Display(Name = "Năm Nhập Học")]
+        [Range(1900, 2100, ErrorMessage = "Năm nhập học không hợp lệ")]
+        public int NamNhapHoc { get; set; }
+
+        [Required(ErrorMessage = "Năm ra trường là bắt buộc")]
+        [Display(Name = "Năm Ra Trường")]
+        [Range(1900, 2100, ErrorMessage = "Năm ra trường không hợp lệ")]
+        public int NamRaTruong { get; set; }
+
+        [Required(ErrorMessage = "Số lượng sinh viên là bắt buộc")]
+        [Display(Name = "Số Lượng Sinh Viên")]
+        [Range(1, int.MaxValue, ErrorMessage = "Số lượng sinh viên phải lớn hơn 0")]
+        public int SoLuongSinhVien { get; set; }
+    }
+}
