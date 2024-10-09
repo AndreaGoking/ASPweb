ea707ecf16ed2f75a6a2c40eac14b1f5e5e55a0f
AndreaGoking <AndreaGoking@gmail.com>
2024-10-09 15:05:52 +0700

    Hoan thanh bai kiem tra 03

---
+++ BaiKiemTra03_04/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using BaiKiemTra03_04.Data;
+using BaiKiemTra03_04.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BaiKiemTra03_04.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
